629ed2068b9aaddfbdbf3917d230048a1049d6f8
Евгений Кирьянов <evgenijkiranov@MacBook-Air-Evgenij-2.local>
2026-01-07 12:31:05 +0300

    Add subscribe button on /b/post

--- src/routes/b/post/[id]/+page.svelte
+++ src/routes/b/post/[id]/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Header from '$lib/components/ui/layout/pages/Header.svelte'
   import Post from '$lib/components/lemmy/post/Post.svelte'
+  import { Button } from 'mono-svelte'
   import { backendPostToPostView, buildBackendPostPath } from '$lib/api/backend'
 
   export let data
 
   const postView = backendPostToPostView(data.post)
+  const subscribeUrl = data.post.channel_url ?? data.post.author?.channel_url ?? null
 </script>
 
 <div class="flex flex-col gap-6 max-w-3xl">
@@ -25,4 +27,19 @@
     subscribeUrl={data.post.channel_url ?? data.post.author?.channel_url}
     subscribeLabel="Подписаться"
   />
+  {#if subscribeUrl}
+    <Button
+      size="sm"
+      color="primary"
+      href={subscribeUrl}
+      target="_blank"
+      rel="noreferrer"
+      class="h-10 !min-h-[2.5rem] max-w-max"
+    >
+      <span class="inline-flex items-center gap-2 text-white">
+        <img src="/img/logos/telegram_logo.svg" alt="Telegram" class="w-4 h-4" />
+        Подписаться на телеграм автора
+      </span>
+    </Button>
+  {/if}
 </div>
