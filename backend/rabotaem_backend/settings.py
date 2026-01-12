9e896b4c3e18bb959152187a5941b8c0b800e0b3
Евгений Кирьянов <evgenijkiranov@MacBook-Air-Evgenij-2.local>
2026-01-12 14:28:07 +0300

    Show admin metrics dashboard

--- backend/rabotaem_backend/settings.py
+++ backend/rabotaem_backend/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = "rabotaem_backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
