7858b43f25f7a5da0b18e5ed59ed66d04504fa06
Евгений Кирьянов <evgenijkiranov@MacBook-Air-Evgenij-2.local>
2026-02-03 16:59:23 +0300

    Add 30-day likes, dislikes, comments to admin metrics

--- backend/feeds/templatetags/admin_metrics.py
+++ backend/feeds/templatetags/admin_metrics.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from django import template
 from django.utils import timezone
 
-from feeds.models import Author, Post
+from feeds.models import Author, Post, PostComment, PostLike
 
 register = template.Library()
 
@@ -22,6 +22,11 @@ def get_admin_metrics() -> dict:
     total_posts = posts_qs.count()
     posts_last_30 = posts_qs.filter(created_at__gte=since).count()
     authors_total = Author.objects.filter(is_blocked=False).count()
+    likes_last_30 = PostLike.objects.filter(created_at__gte=since, value__gt=0).count()
+    dislikes_last_30 = PostLike.objects.filter(created_at__gte=since, value__lt=0).count()
+    comments_last_30 = PostComment.objects.filter(
+        created_at__gte=since, is_deleted=False
+    ).count()
 
     avg_posts_per_author = 0.0
     if authors_total:
@@ -32,4 +37,7 @@ def get_admin_metrics() -> dict:
         "posts_total": total_posts,
         "posts_last_30": posts_last_30,
         "avg_posts_per_author": avg_posts_per_author,
+        "likes_last_30": likes_last_30,
+        "dislikes_last_30": dislikes_last_30,
+        "comments_last_30": comments_last_30,
     }
