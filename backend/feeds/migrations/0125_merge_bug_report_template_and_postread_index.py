5bb33b7f132e2b872347455b6397919e61467539
Евгений Кирьянов <evgenijkiranov@MacBook-Air-Evgenij-2.local>
2026-05-13 16:12:02 +0300

    Add merge migration for feeds 0124 conflict

---
+++ backend/feeds/migrations/0125_merge_bug_report_template_and_postread_index.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("feeds", "0124_add_bug_report_template_type"),
+        ("feeds", "0124_postread_user_post_idx"),
+    ]
+
+    operations = []
